52a31c30dcd0dae7b8de9447abe8fe3edcd3c10b
seanyang816 <seanyang816@gmail.com>
2023-10-23 01:03:09 +0800

    Initialize Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-unused-vars": "warn",
+    "semi": [2, "never"],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    "object-curly-spacing": ["error", "always"],
+    "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+    "comma-dangle": ["error", "never"],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+    'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
+    'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
+    'max-len': ['error', { 'code': 100 }],
+    'quotes': ['error', 'single'],
+    "jsx-quotes": ["error", "prefer-single"],
+  },
+}
